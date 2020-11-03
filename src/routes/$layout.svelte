6dc5b5f93ea47861f7a013705ba3ea30889720e3
Brian Ketelsen <mail@bjk.fyi>
2020-11-03 12:11:34 -0500

    first post

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Counter from '$components/Counter.svelte';
+</script>
+
+<main>
+    <div><a href="/yourmom">Your Mom</a></div>
+    <slot></slot>
+    <div><a href="/mymom">My Mom</a></div>
+</main>
+
+<style>
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+
+
+	@media (min-width: 480px) {
+		h1 {
+			max-width: none;
+		}
+
+		p {
+			max-width: none;
+		}
+	}
+</style>
