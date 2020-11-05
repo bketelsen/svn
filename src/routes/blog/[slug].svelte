df5e129f8ade40fd76dcd87d9fb5e45f4a78678a
Brian Ketelsen <mail@bjk.fyi>
2020-11-04 19:37:09 -0500

    unpolished posts

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -15,5 +15,23 @@
   export let page;
 </script>
 
+<svelte:head>
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content={page.title} />
+  <meta property="og:description" content={page.description} />
+  <meta property="og:url" content="http://www.yoursite.com/yourcontent.html" />
+  <meta property="og:site_name" content="Brian Ketelsen" />
+  <meta property="og:image" content="http://www.yoursite.com/yourimage.jpg" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={page.title} />
+  <meta name="twitter:description" content={page.description} />
+  <meta name="twitter:site" content="@bketelsen" />
+  <meta name="twitter:image" content="http://www.yoursite.com/yourimage.jpg" />
+  <meta name="twitter:creator" content="@bketelsen" />
+  <title>{page.title}</title>
+  <meta name="description" content={page.description} />
+</svelte:head>
+
 <h1>{page.title}</h1>
 {@html page.contents}
