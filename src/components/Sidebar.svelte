681d0eab3a06bfc8d63db85c32043c6393f41a88
Brian Ketelsen <mail@bjk.fyi>
2020-11-06 14:09:08 -0500

    moar

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,18 +1,16 @@
-<!-- <script>
+<!--<script>
 	
-</script>
+</script><style>
 			
-<style>
-			
-</style> -->
-  <div class="side">
-    <h2>About Me</h2>
-    <h5>Photo of me:</h5>
-    <div class="fakeimg" style="height:200px;">Image</div>
-    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-    <h3>More Text</h3>
-    <p>Lorem ipsum dolor sit ame.</p>
-    <div class="fakeimg" style="height:60px;">Image</div><br />
-    <div class="fakeimg" style="height:60px;">Image</div><br />
-    <div class="fakeimg" style="height:60px;">Image</div>
-  </div>
+</style>-->
+<div class="side">
+  <h2>About Me</h2>
+  <h5>Photo of me:</h5>
+  <div class="fakeimg" style="height:200px;" />
+  <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+  <h3>More Text</h3>
+  <p>Lorem ipsum dolor sit ame.</p>
+  <div class="fakeimg" style="height:60px;" /><br />
+  <div class="fakeimg" style="height:60px;" /><br />
+  <div class="fakeimg" style="height:60px;" />
+</div>
