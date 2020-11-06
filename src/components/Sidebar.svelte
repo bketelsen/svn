4be2df1a732f287be2ef0543aef86843cb8e434a
Brian Ketelsen <mail@bjk.fyi>
2020-11-06 14:41:31 -0500

    text

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -8,8 +8,12 @@
   <h5>Photo of me:</h5>
   <div class="fakeimg" style="height:200px;" />
   <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-  <h3>More Text</h3>
-  <p>Lorem ipsum dolor sit ame.</p>
+  <h3>This Site</h3>
+  <p>
+    This website is intentionally built to be as light and fast as possible.
+    Bells and whistles are gone, and in their stead I've used minimal styles and
+    the slightest sprinkle of Javascript.
+  </p>
   <div class="fakeimg" style="height:60px;" /><br />
   <div class="fakeimg" style="height:60px;" /><br />
   <div class="fakeimg" style="height:60px;" />
